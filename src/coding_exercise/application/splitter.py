8d0490b9c45de42557ad8982855a167800c6766c
Mitesh Raval <mitesh22love@gmail.com>
2024-08-02 11:51:26 +1000

    split() method updated according to coding exercise specs

--- src/coding_exercise/application/splitter.py
+++ src/coding_exercise/application/splitter.py
@@ -3,12 +3,51 @@ from coding_exercise.domain.model.cable import Cable
 
 class Splitter:
 
-    def __validate(self):
+    def __validate(self, cable_length, times):
         valid = True
+
+        if cable_length <= 2 or cable_length >= 1024:
+            valid = False
+
+        if times < 1 or times > 64:
+            valid = False
+
+        if cable_length // (times + 1) < 1:
+            valid = False
+
         if not valid:
             raise ValueError
 
     def split(self, cable: Cable, times: int) -> list[Cable]:
-        self.__validate()
+        self.__validate(cable.length, times)
 
-        return []
+        equal_parts = times + 1
+        parts_length, rem_cable_length = divmod(cable.length, equal_parts)
+
+        # initialize local var, defaulting to zero if there is no remaining
+        # last part
+        last_part_length = 0
+
+        if rem_cable_length > 0:
+            total_new_parts, last_part_length = divmod(rem_cable_length, parts_length)
+            equal_parts += total_new_parts
+
+        if last_part_length > 0:
+            total_parts = equal_parts + 1
+        else:
+            total_parts = equal_parts
+
+        # calculate width for cable name formatting with zero filled, right justified id
+        part_id_width = len(str(total_parts))
+
+        result = [
+            Cable(parts_length, f"{cable.name}-{i:0{part_id_width}}")
+            for i in range(equal_parts)
+        ]
+
+        if last_part_length > 0:
+            result.append(
+                Cable(last_part_length, f"{cable.name}-{equal_parts:0{part_id_width}}")
+            )
+
+        return result
